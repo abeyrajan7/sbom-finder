c4afffa4a7885dd951af292f147b1626be26420c
Abey K Rajan <abeyrajan7@gmail.com>
2025-04-20 01:39:14 -0400

    changed server address

--- sbom-finder-backend/src/main/java/com/sbomfinder/config/WebConfig.java
+++ sbom-finder-backend/src/main/java/com/sbomfinder/config/WebConfig.java
@@ -1,21 +1,21 @@
 package com.yourcompany.yourapp.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Allow all endpoints
-                .allowedOrigins(
-                    "https://sbom-finder-clean.vercel.app/dashboard", 
-                    "http://localhost:3000"
-                    )
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true); 
+        registry.addMapping("/**")
+            .allowedOrigins(
+                "https://sbom-finder-clean.vercel.app",
+                "http://localhost:3000"
+            )
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*")
+            .allowCredentials(true);
     }
 }
